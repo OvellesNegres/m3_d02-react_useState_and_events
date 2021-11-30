7409f39e75c3e43823061810ff26962a6036d9de
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-30 14:40:57 +0100

    starter code

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -1,20 +1,5 @@
 function WeatherWidget(props) {
-  let temperature = props.celsius;
-  const unitIsFahrenheit = props.unit === "F";
-
-  if (unitIsFahrenheit) {
-    temperature = props.celsius * 1.8 + 32;
-  }
-
-  return (
-    <div className="WeatherWidget">
-      <span>{props.icon}</span>
-      <h3>{props.city}</h3>
-      <h2>
-        {temperature} °{props.unit}
-      </h2>
-    </div>
-  );
+  return null
 }
 
 export default WeatherWidget;
